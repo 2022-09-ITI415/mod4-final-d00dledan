ffe57f79cb172187d86d9991a7cb2c5e2cbf2a0a
d00dledan <djc382@scarletmail.rutgers.edu>
2022-12-16 14:32:50 -0500

    Open door with left click

---
+++ Assets/InteractScript.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class InteractScript : MonoBehaviour
+{
+
+    public float interactDistance = 5f;
+
+  
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0)) 
+        {
+            Ray ray = new Ray(transform.position, transform.forward);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, interactDistance)) {
+                if (hit.collider.CompareTag("Door"))
+                { 
+                    hit.collider.transform.parent.GetComponent<Door>().ChangeDoorState();
+                }
+            } 
+
+        }
+    }
+}
