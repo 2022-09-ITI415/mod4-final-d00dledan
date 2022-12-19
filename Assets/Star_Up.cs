83b022f78fac08fc49fedec6dfb6a54ee660142f
d00dledan <djc382@scarletmail.rutgers.edu>
2022-12-18 19:12:01 -0500

    Final

--- Assets/Star_Up.cs
+++ Assets/Star_Up.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 
 public class Star_Up : MonoBehaviour
 {
+
+    [SerializeField] private AudioSource doorOpenAudioSource = null;
+
     private void OnTriggerEnter()
     {
         GameManager.instance.Win();
+        doorOpenAudioSource.Play();
         Destroy(gameObject);
     }
 }
